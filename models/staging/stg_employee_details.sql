47deca3caaf6d661128ecb09fdf16c9c9766b264
Geovanny <geovannyalexander07@gmail.com>
2025-04-23 17:15:41 -0500

    Learn about materialization

--- models/staging/stg_employee_details.sql
+++ models/staging/stg_employee_details.sql
@@ -1,6 +1,18 @@
+{{
+  config(
+    materialized = 'incremental',
+    unique_key = 'id',
+    merge_update_columns = ['position', 'department']
+  )
+}}
+
 select
   *
 
-from {{ source('employee_source', 'employee') }}
+from {{ source('employee_source', 'employee_details') }}
 
-where salary > 50000
+{% if is_incremental() %}
+
+  where received_at > (select max(received_at) from {{this}})
+
+{% endif %}
